815f3e2cbee044871508cade9d328fa08223c648
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-14 13:38:46 +0100

    Update tenancy.php

--- config/tenancy.php
+++ config/tenancy.php
@@ -40,7 +40,7 @@ return [
      * Database tenancy config. Used by DatabaseTenancyBootstrapper.
      */
     'database' => [
-        'central_connection' => env('DB_CONNECTION', 'central'),
+        'central_connection' => env('DB_CONNECTION', 'system'),
 
         /**
          * Connection used as a "template" for the dynamically created tenant database connection.
